4355eff1954bf8ba72ecc8af2d352f4c61e9a5ea
jxshi <2723416450@qq.com>
2026-01-21 13:13:05 +0800

    Calculate the quaternion and package it into imu.orientation

--- src/cpp_pkg/include/cpp_pkg/parse_imu_data.h
+++ src/cpp_pkg/include/cpp_pkg/parse_imu_data.h
@@ -1,6 +1,8 @@
 #ifndef PARSE_IMU_DATA_H_
 #define PARSE_IMU_DATA_H_
 #include "ParseData.h"
+#include <cstring>
+#include <cstdint> 
 
 class ParseImuData : public ParseData
 {
@@ -11,6 +13,15 @@ public:
 
 private:
     void print_imu_data(const DataResult& result);
+    static float InvSqrt(float number);
+    void Quaternion_Solution(DataResult& result);
+
     static const uint8_t IMU_DATA_FLAG = 0x11;
+
+    static constexpr float SAMPLING_FREQ = 20.0f; // 采样频率
+    volatile float twoKp = 1.0f;    // 比例反馈系数
+    volatile float twoKi = 0.0f;    // 积分反馈系数
+    volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;  // 四元数初始值
+    volatile float integralFBx = 0.0f,  integralFBy = 0.0f, integralFBz = 0.0f; // 积分反馈初始值
 };
 #endif // PARSE_IMU_DATA_H_
