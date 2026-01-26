b0599f887f658ac7d2d423aefaa3f884d86a0487
jxshi <2723416450@qq.com>
2026-01-26 09:40:24 +0800

    整合代码

--- src/cpp_pkg/src/serial_data_forward/parse_odom_data.cpp
+++ src/cpp_pkg/src/serial_data_forward/parse_odom_data.cpp
@@ -3,12 +3,24 @@
 
 std::shared_ptr<DataResult> ParseOdomData::decode_frame(const std::string& frame)
 {
+    auto result = std::make_shared<DataResult>();
+    nav_msgs::msg::Odometry& odom = result->data.odom;
 
+    odom.header.frame_id = "odom";
+    odom.child_frame_id = "base_link";
+
+    (void)frame;
+
+    result->is_valid = true;
+    result->data_type = DataType::ODOM_DATA;
+    
+    return result;
 }
 
 void ParseOdomData::process_data(const std::shared_ptr<DataResult>& result, std::shared_ptr<PublisherNode> pub_node)
 {
-
+    (void)result;
+    (void)pub_node;
 } 
 
 uint8_t ParseOdomData::get_data_flag() const
